bbf366fe1c4f6dfcf6fa0ebb1307c6ebe4b7c705
ha-kuku <hakukudh@gmail.com>
2024-12-10 14:47:06 +0900

    feat: 입력 창 만들기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { Console } from '@woowacourse/mission-utils';
+import { INPUT, ERROR } from './Constants/Message.js';
+
 class App {
-  async run() {}
+  getWorkingDate = async () => {
+    const input = await Console.readLineAsync(INPUT.date);
+    return input.trim();
+  };
+
+  getWeekdayWorker = async () => {
+    const input = await Console.readLineAsync(INPUT.weekdayWorker);
+    return input.trim();
+  };
+
+  getHolidayWorker = async () => {
+    const input = await Console.readLineAsync(INPUT.holidayWorker);
+    return input.trim();
+  };
+
+  async run() {
+    try {
+      await this.getWorkingDate();
+      await this.getWeekdayWorker();
+      await this.getHolidayWorker();
+    } catch (err) {
+      Console.print(err.message);
+      throw err;
+    }
+  }
 }
 
 export default App;
--- src/Constants/Message.js
+++ src/Constants/Message.js
@@ -14,3 +14,5 @@ const PREFIX_ERROR = '[ERROR]';
 const ERROR = {
   invalidInput: `${PREFIX_ERROR} 유효하지 않은 입력 값입니다. 다시 입력해 주세요.`,
 };
+
+export { INPUT, OUTPUT, ERROR };
